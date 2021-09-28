87e4569b5c3eccfddd3444c14f3a39e548ba1d90
chr1s78 <ayahirano2014@126.com>
2021-09-29 00:27:44 +0800

    Initial Commit

---
+++ SearchFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchFlow
+//
+//  Created by Chr1s on 2021/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchFlow/SearchFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchFlowApp.swift
+//  SearchFlow
+//
+//  Created by Chr1s on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct SearchFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
